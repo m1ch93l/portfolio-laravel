fb7de1db6a4afba4f5c556e349d18764712d01fe
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-11-23 16:38:08 +0800

    Add main header.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,30 @@
 				</div>
 			</div>
 
+			<!-- Navbar -->
+			<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+				<!-- Left navbar links -->
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+					</li>
+					<li class="nav-item d-none d-sm-inline-block">
+						<button type="button" class="btn btn-outline-success mx-2">HOME</button>
+					</li>
+					<li class="nav-item d-none d-sm-inline-block">
+						<button type="button" class="btn btn-outline-success mx-2">PROJECT</button>
+					</li>
+				</ul>
+
+				<!-- Right navbar links -->
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item d-none d-sm-inline-block">
+
+					</li>
+				</ul>
+			</nav>
+			<!-- /.navbar -->
+
 		</div>
 
 	</body>
